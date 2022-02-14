98f8c4a3e42c3a6559d6fefde9e4c2f8a3a2fa16
dcpp <dmitriy.dolinin.v@gmail.com>
2022-02-14 18:39:46 +0300

    deposit

--- test/vault.spec.ts
+++ test/vault.spec.ts
@@ -1,10 +1,16 @@
-
-import {Address, Cell} from 'ton'
+import {Address, Cell, CellMessage, CommonMessageInfo, InternalMessage} from 'ton'
 import {readFile} from 'fs/promises'
 import {SmartContract} from 'ton-contract-executor'
 import BN from "bn.js"
 
-const myAddress = Address.parseFriendly('EQD4FPq-PRDieyQKkizFTRtSDyucUIqrj0v_zXJmqaDp6_0t').address
+const contractAddress = Address.parse('EQD4FPq-PRDieyQKkizFTRtSDyucUIqrj0v_zXJmqaDp6_0t')
+const myAddress = Address.parse('kQChmZFQneZ6AUXG-eBkqcw_8WI6HQTD4i0Z9OTpJIYDH03t')
+
+const getContract = async (source: string) => {
+    let data = new Cell()
+    data.bits.writeUint(0, 1)        
+    return await SmartContract.fromFuncSource(source, data, { getMethodsMutate: true })
+}
 
 describe('TON Vault', () => {
     let source: string
@@ -14,25 +20,44 @@ describe('TON Vault', () => {
     })
 
     it('should return total balance', async () => {
-        let dataCell = new Cell()
-        dataCell.bits.writeUint(0, 1)        
-        let contract = await SmartContract.fromFuncSource(source, dataCell, { getMethodsMutate: true })
+        let contract = await getContract(source)
         let res = await contract.invokeGetMethod('balance', []);
         expect(res.result[0]).toBeInstanceOf(BN)
         expect(res.result[0].toNumber()).toEqual(1000)
-        //let address = new BN(myAddress.hash)
-        //let res = await contract.invokeGetMethod('balanceof', [{ type: 'int', value: address.toString(10) }])
     })
 
-    it('should return balance of ', async () => {
-        let dataCell = new Cell()
-        dataCell.bits.writeUint(0, 1)        
-        let contract = await SmartContract.fromFuncSource(source, dataCell, { getMethodsMutate: true })
+    it('should return balance of user', async () => {
+        let contract = await getContract(source)
         let address = new BN(myAddress.hash)
         let res = await contract.invokeGetMethod('balanceof',
-            [{ type: 'int', value: myAddress.workChain.toString() }, { type: 'int', value: address.toString() }])
+            [{ type: 'int', value: myAddress.workChain.toString(10) }, { type: 'int', value: address.toString(10) }])
+        expect(res.result[0]).toBeInstanceOf(BN)
+        expect(res.result[0].toNumber()).toEqual(0)
+    })
+
+    it('should deposit by user and check user balance', async () => {
+        let contract = await getContract(source)
+
+        let messageBody = new Cell()
+        messageBody.bits.writeUint(1, 32) // op
+        messageBody.bits.writeUint(2, 64) // query_id
+
+        let res = await contract.sendInternalMessage(new InternalMessage({
+            to: contractAddress,
+            from: myAddress,
+            value: new BN(10),
+            bounce: false,
+            body: new CommonMessageInfo({ body: new CellMessage(messageBody) })
+        }))
+
+        res = await contract.invokeGetMethod('balance', []);
+        expect(res.result[0]).toBeInstanceOf(BN)
+        expect(res.result[0].toNumber()).toEqual(1000)
+
+        res = await contract.invokeGetMethod('balanceof',
+            [{ type: 'int', value: myAddress.workChain.toString(10) },
+             { type: 'int', value: (new BN(myAddress.hash)).toString(10) }])
         expect(res.result[0]).toBeInstanceOf(BN)
         expect(res.result[0].toNumber()).toEqual(0)
     })
 })
-
