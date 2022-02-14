41341fd637c62f151e51c04a1640afe7ac3b7253
dcpp <dmitriy.dolinin.v@gmail.com>
2022-02-14 16:29:37 +0300

    balanceof test

--- test/vault.spec.ts
+++ test/vault.spec.ts
@@ -1,27 +1,38 @@
 
-import {Address, Cell} from 'ton';
-import {readFile} from 'fs/promises';
-//import BN from "bn.js";
-import {SmartContract} from 'ton-contract-executor';
+import {Address, Cell} from 'ton'
+import {readFile} from 'fs/promises'
+import {SmartContract} from 'ton-contract-executor'
+import BN from "bn.js"
 
 const myAddress = Address.parseFriendly('EQD4FPq-PRDieyQKkizFTRtSDyucUIqrj0v_zXJmqaDp6_0t').address
 
 describe('TON Vault', () => {
-    let source: string;
+    let source: string
 
     beforeAll(async () => {
-        source = (await readFile('./contracts/vault.fc')).toString('utf-8');
-    });
+        source = (await readFile('./contracts/vault.fc')).toString('utf-8')
+    })
 
     it('should return total balance', async () => {
         let dataCell = new Cell()
         dataCell.bits.writeUint(0, 1)        
-        let contract = await SmartContract.fromFuncSource(source, dataCell, { getMethodsMutate: true });
-        console.log(contract);
+        let contract = await SmartContract.fromFuncSource(source, dataCell, { getMethodsMutate: true })
         let res = await contract.invokeGetMethod('balance', []);
+        expect(res.result[0]).toBeInstanceOf(BN)
+        expect(res.result[0].toNumber()).toEqual(1000)
         //let address = new BN(myAddress.hash)
-        //let res = await contract.invokeGetMethod('balanceof', [{ type: 'int', value: address.toString(10) }]);
-        //expect(res.result[0]).toBeInstanceOf(Cell)
-    });
-});
+        //let res = await contract.invokeGetMethod('balanceof', [{ type: 'int', value: address.toString(10) }])
+    })
+
+    it('should return balance of ', async () => {
+        let dataCell = new Cell()
+        dataCell.bits.writeUint(0, 1)        
+        let contract = await SmartContract.fromFuncSource(source, dataCell, { getMethodsMutate: true })
+        let address = new BN(myAddress.hash)
+        let res = await contract.invokeGetMethod('balanceof',
+            [{ type: 'int', value: myAddress.workChain.toString() }, { type: 'int', value: address.toString() }])
+        expect(res.result[0]).toBeInstanceOf(BN)
+        expect(res.result[0].toNumber()).toEqual(0)
+    })
+})
 
